80566b3419ec8a72575832537dfcb69109e24a70
Minori Hashimoto <minori.fh@gmail.com>
2019-09-02 14:13:39 -0700

    add initial pages in pages and routing in app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
+import Home from './pages/Home/Home'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={Home} />
+        <Route exact path="/program-applications" component={Home} />
+        <Route exact path="/jobs-at-leap" component={Home} />
+        <Route exact path="/swimming" component={Home} />
+        <Route exact path="/news-and-events" component={Home} />
+        <Route exact path="/volunteer-and-intern" component={Home} />
+        <Route exact path="/contact" component={Home} />
+        <Route exact path="/give-to-leap" c  omponent={Home} />
+        <Route path="*" component={Home} />
+      </Switch> 
+    </Router>
+  )   
 }
+
 
 export default App;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+
+    render(){
+        return(
+            <div>
+                Hello this is the home page
+            </div>
+        )
+    }
+}
+
+export default Home;
